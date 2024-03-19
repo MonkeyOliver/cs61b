568878d39ebce2bd48b65eaaf64974d44134bad9
MonkeyOliver <wxy007@outlook.com>
2024-03-19 22:25:48 +0800

    fix DebugExercise2

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -12,7 +12,7 @@ public class DebugExercise2 {
            step out button because you're not going to learn anything. */
         int z = ~(b - a) >> 31;
 
-        int max = b & w | a & z;
+        int max = a & w | b & z;
         return max;
     }
 
@@ -46,8 +46,8 @@ public class DebugExercise2 {
         }
         int[] returnArray = new int[a.length];
         for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = max(-a[i], -b[i]);
-            returnArray[i] = -biggerValue;
+            int biggerValue = max(a[i], b[i]);
+            returnArray[i] = biggerValue;
         }
 
         return returnArray;
